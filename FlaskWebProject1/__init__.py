3a2f9911421dcfeb9d5b223c023aefdb6fbb99b9
Waruna Yapamudiyanselage <warunayapa619@gmail.com>
2017-01-30 15:52:24 -0500

    add test

--- FlaskWebProject1/__init__.py
+++ FlaskWebProject1/__init__.py
@@ -1,14 +1,10 @@
 """
 The flask application package.
 """
-'''
-from flask_cors import CORS, cross_origin
-'''
+
 from flask import Flask
 
 app = Flask(__name__)
-'''
-CORS(app)
-'''
+
 
 import FlaskWebProject1.views
