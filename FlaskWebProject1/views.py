b679a28009d8f731929287386c8b18b216acd3b0
Waruna <warunayapa619@gmail.com>
2017-01-30 17:42:28 -0500

    test

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -37,8 +37,8 @@ def about():
         message='Your application description page.'
     )
 
-@app.route('/getmethod/<jsdata>')
-def get_javascript_data(jsdata):
-    jsdata = "TEST2"
-    return json.dumps(jsdata)
+@app.route('/postmethod', methods = ['POST'])
+def get_post_javascript_data():
+    jsdata = request.form['javascript_data']
+    return jsdata
 
