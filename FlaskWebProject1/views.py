b9b9f04526dcd3e077922a9e7500336fb16416e7
Waruna <warunayapa619@gmail.com>
2017-02-01 02:04:31 -0500

    added editor

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -58,6 +58,7 @@ def postmethod():
     path = './user_code/' + str(name) 
     py_file = path + '.py'
     psu_file = path + '.psu'
+    out_file = path + '.out'
     psu_file = os.path.abspath(psu_file)
 
     with open(psu_file, 'a+') as f:
@@ -66,16 +67,19 @@ def postmethod():
     
 
     command = 'pseudo.py ' + psu_file
-    print command
+    
     ps = pseudo.main(command)
    
     with open(py_file, 'r') as file:
         python_code = file.read()
     
-
-
-
-    '''
+    command = 'python ' + py_file + ' >> ' + out_file
+    
+    os.system(command)
+    with open(out_file, 'r') as file:
+        output = file.read()
+    
+    
 
     send = {}
     send['python'] = python_code
@@ -85,9 +89,10 @@ def postmethod():
 
     
 
-
+    data = {}
     python_code = python_code.replace('\n', '&&newline&&')
-    return json.dumps(python_code)
-    '''
-
-    return json.dumps("HELLO")
+    output = output.replace('\n', '&&newline&&')
+    data['python'] = python_code
+    data['output'] = output
+    return json.dumps(data)
+    
--- test.py
+++ test.py
@@ -10,14 +10,19 @@ import pseudo
 psu_path = './user_code/7f59f600-76c1-40ca-821b-4f17f204af79.psu'
 
 psu_path = os.path.abspath(psu_path)
-
-
+path = './user_code/'
+py_path = '/home/waruna/Documents/projects/pseudo_web/PseudoWebsite/user_code/7f59f600-76c1-40ca-821b-4f17f204af79.py'
 command = 'pseudo.py ' + psu_path
 
 ps = pseudo.main(command)
 
+command = 'python ' + py_path + ' >> ' + path + '7f59f600-76c1-40ca-821b-4f17f204af79.out'
+print command
+
 
 '''
+
+
 with open(py_file, 'r') as file:
         test = file.read()
 
