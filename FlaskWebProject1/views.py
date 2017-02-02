d920c7d73dbb34b24749c547037b34e6aa152683
Waruna <warunayapa619@gmail.com>
2017-02-02 01:35:54 -0500

    update

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -80,7 +80,7 @@ def postmethod():
     prog = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
    
     
-    out, err = prog.communicate()
+    
     '''
     prog.wait()
     '''
